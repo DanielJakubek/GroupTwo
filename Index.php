7988f8388cdfedae45aba1388189412edf20868b
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-01-28 11:13:42 +0000

    asdasasd

---
+++ Index.php
@@ -0,0 +1,197 @@
+
+<html lang="en">
+  <head>
+    <!-- CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="masterCSS.css">
+
+    <meta charset="utf-8" /> <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Index</title>
+  </head>
+
+  <body>
+    <!-- Navigation bar -->
+    <nav class="navbar">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="indexTwo.php" id="SustainNavImg">
+          <img src="Images\AgileUnBW.png" alt="UnLogoSustainB&W" class="d-inline-block align-text-top">
+        </a>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search. . ." aria-label="Search" id="searchForm">
+          <img src="Images\noteBook.png" alt="listNoteBook" class="d-inline-block align-text-top" id="noteBookPng">
+        </form>
+      </div>
+    </nav>
+
+    <!-- Main Content/body -->
+    <content>
+      <div class="bg-image">
+        <div class="container-flex" id="intermissionArea">
+          <h1> Dundee [Make this look better, bottom lef?] </h1>
+        </div>
+
+        <!-- First carousel -->
+        <div class="container-flex" id="carouselArea">
+          <div id="firstCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="d-flex justify-content-center">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <img src="Images\NoPoverttyBW.jpg" class="d-block w-100" alt="No Poverty">
+                    </div>
+                    <div class="col-md-4">
+                      <img src="Images\NoHungerBW.jpg" class="d-block w-100" alt="Zero Hunger">
+                    </div>
+                    <div class="col-md-4">
+                      <img src="Images\GoodHealthBW.jpg" class="d-block w-100" alt="...">
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <img src="Images\4.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-md-4">
+                      <img src="Images\5.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-md-4">
+                      <img src="Images\6.jpg" class="d-block w-100" alt="...">
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <img src="Images\7.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-md-4">
+                      <img src="Images\8.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-md-4">
+                      <img src="Images\9.jpg" class="d-block w-100" alt="...">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#firstCarousel" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#firstCarousel" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+
+          <div class="d-flex justify-content-center" id="sliderCounter">
+            <button type="button" data-bs-target="#firstCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#firstCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#firstCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+        </div>
+
+        <!-- White Space -->
+        <div class="container-flex" id="intermissionArea">
+        </div>
+
+        <!-- Second Carousel -->
+        <div class="container-flex" id="carouselArea">
+          <div id="secondCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="d-flex justify-content-center">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <img src="Images\10.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-md-4">
+                      <img src="Images\11.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-md-4">
+                      <img src="Images\12.jpg" class="d-block w-100" alt="...">
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <img src="Images\13.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-md-4">
+                      <img src="Images\14.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-md-4">
+                      <img src="Images\15.jpg" class="d-block w-100" alt="...">
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <img src="Images\16.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-md-4">
+                      <img src="Images\17.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-md-4">
+                      <img src="Images\1.jpg" class="d-block w-100" alt="...">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#secondCarousel" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#secondCarousel" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+
+          <div class="d-flex justify-content-center" id="sliderCounter">
+            <button type="button" data-bs-target="#secondCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#secondCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#secondCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+        </div>
+
+        <!-- White Space -->
+        <div class="container-flex" id="intermissionArea">
+        </div>
+
+      </div>
+    </content>
+
+
+    <!-- Footer -->
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-md-12 mb-4 mb-md-0">Sample Text
+          </div>
+          <div class="col-lg-6 col-md-12 mb-4 mb-md-0">Sample Text
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid" id="copyrightText"> © 2022 DeepFreezeInc </div>
+    </footer>
+  </body>
+
+
+
+
+
+
+
+
+
+
+
+  <!-- JavaScript-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</html>
